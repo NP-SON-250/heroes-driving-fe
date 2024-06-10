4f28e79db3670baa0fd6098a3a4bc3c3ee6406f3
NP-SON <hakizimanaalexis123@gmail.com>
2024-06-10 10:02:21 +0300

    Add admin questions, paymeny report, and setting components

---
+++ src/components/Admin/Payments/Payments.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Footer from '../../Footer/Footer'
+const Payments = () => {
+  return (
+    <div><Footer/></div>
+  )
+}
+
+export default Payments
---
+++ src/components/Admin/Settings/Settings.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Footer from '../../Footer/Footer'
+
+const Settings = () => {
+  return (
+    <div><Footer/></div>
+  )
+}
+
+export default Settings
